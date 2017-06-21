a9e61d9c916c8bfbed4f3320a42868ff1d698520
neetucompiler <neetucompiler@gmail.com>
2017-06-21 05:49:55 -0400

    fixed body-parser and started server on default port

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var server = http.createServer(app)
 var io = require('socket.io')(server)
 var apiai = require('apiai')
 var apiapp = apiai('dde8e7dda0a9453da17fcf25cd88765f')
+var bodyParser = require('body-parser')
 
 var options = {
   sessionId: '1111'
@@ -16,7 +17,8 @@ app.use(express.Router())
 app.use(express.static(path.join(__dirname, '/build/production')))
 server.listen(process.env.PORT || 3000)
 console.log(path.join(__dirname, '/build/production/index.html'))
-app.use(express.bodyParser());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.get('/', function(req, res) {
   res.sendfile(path.join(__dirname, '/build/production/index.html'))
 })
@@ -65,4 +67,12 @@ io.on('connection', function(socket) {
   socket.on('disconnect', function() {
     console.log('A user is disconnected')
   })
+})
+
+app.set('port', process.env.port || 3000)
+server.listen(app.get('port'), function (err){
+  if(err){
+    console.log('Error: ' + err)
+  }
+  console.log('Server started on port: ' + app.get('port'))
 }) 
